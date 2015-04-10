b637b670a01a0afce9be21545c85433ebe66913f
Carl Swanson <cetswanson@gmail.com>
2015-04-10 16:09:25 -0400

    Set up routes for uploading photos

--- app/controllers/photo.rb
+++ app/controllers/photo.rb
@@ -1,26 +1,24 @@
 require "base64"
-# require 'rubygems'
-# require 'sinatra'
-# require 'haml'
+require 'rubygems'
+require 'sinatra'
+require 'haml'
 
-# # Handle GET-request (Show the upload form)
-# get "/upload" do
-#   haml :upload
-# end
+#Handle GET-request (Show the upload form)
+get "/photos/upload" do
+  haml :'photo/upload'
+end
 
-# # Handle POST-request (Receive and save the uploaded file)
-# post "/upload" do
-#   File.open('uploads/' + params['myfile'][:filename], "w") do |f|
-#     f.write(params['myfile'][:tempfile].read)
-#   end
-#   return "The file was successfully uploaded!"
-# end
-
-#to do incorporate this into routes
-
+#Handle POST-request (Receive and save the uploaded file)
+post "/photos/upload" do
+  upload_pic = File.open(params['upload_photo'][:tempfile], "rb").read
+  new_photo = Photo.create(image: upload_pic, album_id: 1)
+  redirect "/photos/#{new_photo.id}"
+end
 
 get '/photos/:id' do |photo_id|
   photo_binary = Photo.find(photo_id).image
   @photo = Base64.encode64(photo_binary)
   erb :'photo/show'
 end
+
+
