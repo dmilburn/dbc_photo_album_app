62037a638129fb9b0b6a7f23f7c4a5b9f7a8d458
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-10 16:14:08 -0400

    User now enters a login and is moved to the album page

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,9 +1,22 @@
+enable :sessions
+
+
 get '/' do
-  erb :welcome
+  redirect '/login'
 end
 
 get '/login' do
+  erb :'auth/login'
+end
 
+post '/login' do
+  user = User.find_by(name: params[:name])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/albums'
+  else
+    redirect '/login'
+  end
 end
 
 get '/signup' do
