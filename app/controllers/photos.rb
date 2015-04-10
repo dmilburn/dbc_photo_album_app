43c3eb14eb8ebd397a5ea47999dfcababf4df0b3
Carl Swanson <cetswanson@gmail.com>
2015-04-10 16:20:13 -0400

    rename photo controller to photos

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -1,23 +1,31 @@
 # photo routes
 
+require "base64"
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+#Handle GET-request (Show the upload form)
+get "/photos/upload" do
+  haml :'photo/upload'
+end
+
+#Handle POST-request (Receive and save the uploaded file)
+post "/photos/upload" do
+  upload_pic = File.open(params['upload_photo'][:tempfile], "rb").read
+  new_photo = Photo.create(image: upload_pic, album_id: 1)
+  redirect "/photos/#{new_photo.id}"
+end
+
+get '/photos/:id' do |photo_id|
+  photo_binary = Photo.find(photo_id).image
+  @photo = Base64.encode64(photo_binary)
+  erb :'photo/show'
+end
+
 # show all photos
-get '/photos' do
+get '/photos/' do
   "show all photos"
-end
-
-
-#add photo to album
-get '/photos/new' do
-  "new photo"
-end
-
-post '/photos/new' do
-  # validations for new photo creation
-end
-
-# show one photo
-get '/photos/:id' do |id|
-  "show one photo"
 end
 
 #edit photo
