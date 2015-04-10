9b2c613f6b0ef0957a64b82e76085fed2e96d1bf
Charles Green <Charles.Green88@gmail.com>
2015-04-10 15:04:52 -0400

    initial commit. Establishing CRUD route skeleton

---
+++ app/controllers/albums.rb
@@ -0,0 +1,31 @@
+#albums routes
+
+#show all
+get '/albums' do
+"show all albums"
+end
+
+#show one album
+get '/albums/:id' do |id|
+"show an album"
+end
+
+#edit album
+
+get '/albums/:id/edit' do |id|
+  @album = album.find(id)
+  erb: 'albums/edit'
+end
+
+put '/albums/:id/edit' do |id|
+  album = album.find(id)
+  album.update(params[:album])
+  redirect "/album/#{album.id}"
+end
+
+# delete an album
+delete '/albums/:id/delete' do |id|
+  album = Album.find(id)
+  album.destroy
+  redirect '/albums'
+end
---
+++ app/controllers/photos.rb
@@ -0,0 +1,41 @@
+# photo routes
+
+# show all photos
+get '/photos' do
+  "show all photos"
+end
+
+
+#add photo to album
+get '/photos/new' do
+  "new photo"
+end
+
+post '/photos/new' do
+  # validations for new photo creation
+end
+
+# show one photo
+get '/photos/:id' do |id|
+  "show one photo"
+end
+
+#edit photo
+get '/photos/:id/edit' do |id|
+  @photo = Photo.find(id)
+  erb: 'photos/edit'
+end
+
+put '/photos/:id/edit' do |id|
+  photo = Photo.find(id)
+  photo.update(params[:photo])
+  redirect "/photo/#{album.id}"
+end
+
+#delete a photo
+
+delete '/photos/:id/delete' do |id|
+  photo = Photo.find(id)
+  photo.destroy
+  redirect '../albums'
+end
