becde71831c05e513ea744bac734e08691a9badd
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-11 12:18:03 -0400

    Added user validations and flash messages for albums controller

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -7,7 +7,12 @@ require 'haml'
 
 #Handle GET-request (Show the upload form)
 get "/photos/upload" do
-  haml :'photo/upload'
+  if current_user
+    haml :'photo/upload'
+  else
+    flash[:error] = "You do not have permission to see that. Please login."
+    redirect '/login'
+  end
 end
 
 #Handle POST-request (Receive and save the uploaded file)
@@ -22,29 +27,34 @@ post "/photos/upload" do
 end
 
 get '/photos/:id' do |photo_id|
-  @photo_object =  Photo.find(photo_id)
-  photo_binary = @photo_object.image
-  @photo = Base64.encode64(photo_binary)
-  erb :'photo/show'
-end
-
-# show all photos
-get '/photos/' do
-  "show all photos"
+  if current_user
+    @photo_object =  Photo.find(photo_id)
+    photo_binary = @photo_object.image
+    @photo = Base64.encode64(photo_binary)
+    erb :'photo/show'
+  else
+    flash[:error] = "You do not have permission to see that. Please login."
+    redirect '/login'
+  end
 end
 
 #edit photo
 get '/photos/:id/edit' do |id|
-  @photo = Photo.find(id)
-  @albums = Album.all
-  erb :'photo/edit'
+  if current_user
+    @photo = Photo.find(id)
+    @albums = Album.all
+    erb :'photo/edit'
+  else
+    flash[:error] = "You do not have permission to see that. Please login."
+    redirect '/login'
+  end
 end
 
 put '/photos/:id/edit' do |id|
-  photo = Photo.find(id)
-  album = Album.find_by(name: @album_name)
-  photo.update(params[:photo])
-  redirect "/photos/#{photo.id}"
+    photo = Photo.find(id)
+    album = Album.find_by(name: @album_name)
+    photo.update(params[:photo])
+    redirect "/photos/#{photo.id}"
 end
 
 #delete a photo
