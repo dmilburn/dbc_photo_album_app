186def97c4c75b668efcd3b0ebcbd1fa9e5c3680
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-04-11 16:27:25 -0400

    fixed uniquness typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :albums
   validates :password_digest, presence: true
-  validates :name, presence: true, uniqueness: :true
+  validates :name, presence: true, uniqueness: true
 end
