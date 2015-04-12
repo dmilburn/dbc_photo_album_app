c17fe1f921064f49e31d513a1c8c01741bee9d6b
Charles Green <Charles.Green88@gmail.com>
2015-04-12 14:00:06 -0400

    working on albums page layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,12 @@ Album.create(name: "Travel", user_id: 1)
 
 Album.create(name: "Family", user_id: 1)
 Album.create(name: "Selfies", user_id: 1)
-Album.create(name: "Times I fell", user_id: 1)
 
-read_picture = File.open("/Users/charles/Desktop/borat.jpg", 'rb').read
+picture_1 = File.open("/Users/charles/Desktop/borat.jpg", 'rb').read
+picture_2 = File.open("/Users/charles/Desktop/H2H.jpg", 'rb').read  
+# picture_3 = File.open("/Users/charles/Desktop/borat.jpg", 'rb').read
+# picture_4 = File.open("/Users/charles/Desktop/H2H.jpg", 'rb').read
 
-Photo.create(image: read_picture, album_id: 1)
+Photo.create(image: picture_1, album_id: 1)
 
+Photo.create(image: picture_2, album_id: 1)
