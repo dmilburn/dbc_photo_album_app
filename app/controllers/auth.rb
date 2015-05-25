4853ce9c03158d8768601751ff8281054dd3a8b0
Debbie Milburn <deborah.a.milburn@gmail.com>
2015-05-24 21:16:21 -0400

    Refactor signup controller

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -29,18 +29,13 @@ end
 
 post '/signup' do
   user = User.new(params[:user])
-  if empty_sign_up_field?(params[:user])
-    flash[:error] = "You need a username and a password to sign up. Please try again."
-  elsif params[:user][:password] != params[:user][:password_confirmation]
-    flash[:error] = "Your passwords didn't match. Please try again."
-  elsif !user.valid?
-    flash[:error] = "That username has already been chosen. Please try again."
-  else
-    user.save
+  if user.save
     session[:user_id] = user.id
     redirect '/'
+  else
+    flash[:error] = user.errors.full_messages.join(". ")
+    redirect '/signup'
   end
-  redirect '/signup'
 end
 
 get '/logout' do
